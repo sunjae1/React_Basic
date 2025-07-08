e286023db3874c0512422c1d26c597604e41b8fe
sunjae1 <mk35plkr@naver.com>
2025-07-08 09:44:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// index.html 같은거.
+
 function App() {
+
+
+  let posts='강남 고기 맛집';
+  document.getElementById().innerHTML=''; //전통적인 자바스크립트 데이터 바인딩
   return (
+    //리액트 에서는 html이 아닌 좀 이상한 jsx 문법을 사용.(일종의 js라서)
+    // class--> className
+    // 장점 : 리액트는 데이터 바인딩이 쉽다.
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav"> 개발 Blog
+
+      <h4>블로그 글</h4>
+
+     </div>
     </div>
   );
 }
